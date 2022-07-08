cacf39af81ddadeb6fd29345c4cab44e64e00986
VasiliyBogdanov <moproq@icloud.com>
2022-07-08 16:19:13 +0300

    Fix localization

--- task_manager/translations.py
+++ task_manager/translations.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 
 LOGIN = _("Login")
-LOGIN_BUTTON = _("Login")
+LOGIN_BUTTON = _("Yes, login")
 LOGGED_OUT = _('You are logged out')
 LOGGED_IN = _('You are logged in')
